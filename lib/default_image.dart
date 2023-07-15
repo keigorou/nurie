2c37d52cedc2963108438ed950961d98d88a84a3
keigorou <keigorou84@gmail.com>
2023-07-15 18:57:17 +0900

    UI修正

--- lib/default_image.dart
+++ lib/default_image.dart
@@ -10,27 +10,29 @@ class DefaultImage extends StatelessWidget {
     final sizeClass = screen.sizeClass;
 
     return Center(
-      child: Column(
-        children: [
-          SizedBox(height: screen.designH(40)),
-          Container(
-            width: screen.designW(200),
-            height: screen.designH(200),
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-                    fit: BoxFit.contain,
-                    image: AssetImage('assets/image_select_icon.png'))),
-          ),
-          SizedBox(height: screen.designH(40)),
-          Center(
-            child: Text(
-              'カメラやアルバムから\nがぞうをえらぼう',
-              style: TextStyle(
-                  fontSize: sizeClass == ScreenSizeClass.phone ? 28 : 48,
-                  letterSpacing: 8),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: screen.designH(40)),
+            Container(
+              width: screen.designW(200),
+              height: screen.designH(200),
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.contain,
+                      image: AssetImage('assets/image_select_icon.png'))),
             ),
-          )
-        ],
+            SizedBox(height: screen.designH(40)),
+            Center(
+              child: Text(
+                'カメラやアルバムから\nがぞうをえらぼう',
+                style: TextStyle(
+                    fontSize: sizeClass == ScreenSizeClass.phone ? 28 : 48,
+                    letterSpacing: 8),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
