8f6f374394945bc31c4ce38b024a5a93a5daba13
keigorou <keigorou84@gmail.com>
2023-07-15 18:56:27 +0900

    ボタンカラーを追加

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,1 +1,8 @@
+import 'package:flutter/material.dart';
+
 const String mainFont = 'Cherry';
+
+const Color imgPickBtnColor = Color(0xffff9500);
+const Color imgSaveBtnColor = Colors.redAccent;
+const Color imgConvertBtnColor = Colors.redAccent;
+const Color lineThicknessBtnColor = Colors.amber;
